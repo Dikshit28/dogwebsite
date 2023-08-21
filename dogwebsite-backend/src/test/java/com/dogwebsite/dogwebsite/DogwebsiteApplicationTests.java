abd70376149101c63ee60521e777533eb19eab46
Dikshit <dikshit19csu379@ncuindia.edu>
2023-08-21 15:57:09 +0530

    first commit

---
+++ dogwebsite-backend/src/test/java/com/dogwebsite/dogwebsite/DogwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogwebsite.dogwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
